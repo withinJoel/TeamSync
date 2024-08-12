e7579b7c309509e50010e271f79c1f06c573de00
Joel Jolly <128782382+withinJoel@users.noreply.github.com>
2024-08-12 10:34:57 +0530

    update

---
+++ src/main/java/com/example/teamsync/TeamsyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamsyncApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/teamsync/TeamsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
