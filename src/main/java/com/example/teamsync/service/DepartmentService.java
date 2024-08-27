332c0f3543d6a3e5b75d0c908935235b3b9d0f46
Joel Jolly <joeljollyhere@gmail.com>
2024-08-27 10:32:13 +0530

    update

--- src/main/java/com/example/teamsync/service/DepartmentService.java
+++ src/main/java/com/example/teamsync/service/DepartmentService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class DepartmentService {
